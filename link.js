68828066abba75e11e8aa7b6bbd730c7c292e156
CoolCoders345 <86123958+CoolCoders345@users.noreply.github.com>
2022-01-17 19:50:46 -0700

    Add files via upload

---
+++ link.js
@@ -0,0 +1,16 @@
+class Link{
+    constructor(bodyA, bodyB){
+        var lastLink = bodyA.body.bodies.length-2
+        var options = {
+            bodyA: bodyA.body.bodies[lastLink],
+            bodyB:bodyB,
+            length:-10,
+            stiffness:0.01
+        }
+        this.link = Constraint.create(options)
+        World.add(world, this.link)
+    }
+    show(){
+
+    }
+}
--- sketch.js
+++ sketch.js
@@ -6,55 +6,39 @@ const Constraint = Matter.Constraint;
 const Body = Matter.Body;
 const Composites = Matter.Composites;
 const Composite = Matter.Composite;
-var bridge
-var ground, wallR, wallL
-var link,button
-var backIMg, background, zombie, zomIMG
-var stones  = []
-function preload(){
-  backIMg = loadImage("assets/background.png")
-zomIMG = loadImage("assets/zombie.png")
 
-
-}
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let engine;
+let world;
+var ground;
+var rope
+var fruit
+var connector
+function setup() 
+{
+  createCanvas(600,600);
+  frameRate(80);
   engine = Engine.create();
   world = engine.world;
-  frameRate(80);
-  background.addImage(backIMg)
-zombie.addImage(zomIMG)
-zombie.velocityX = 10
-button.createButton("")
-button.position(width-200,height/2-50)
-button.class("breakbutton")
-button.mousePressed(handleButtonPress)
-bridge =new Bridge(30,{x:150,y:windowHeight/2})
-ground = new Base(windowWidth/2, windowHeight-10,windowWidth,10)
-wallR = new Base(10,windowHeight/2,300,100)
-wallL = new Base(2000,windowHeight/2,300,100)
-link = new Link(bridge, wallL)
-drawSprites()
+  ground = new Ground(300,590,600,20);
+  rope = new Rope(6, {x:300,y:50})
+  fruit = Matter.Bodies.circle(300,300,20)
+  Matter.Composite.add(rope.body, fruit)
+connector = new Link(rope, fruit)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
 }
 
-function draw() {
+function draw() 
+{
   background(51);
+  ground.show();
+  rope.show()
+ellipse(fruit.position.x, fruit.position.y, 20)
   Engine.update(engine);
-  bridge.show()
-  ground.show()
-  wallR.show()
-  wallL.show()
-  for (var i = 0; i<=8;i++){
-    var x =  random (width/2-200,width/2+300);
-    var y = random (-10,140);
-    var stone = new Stone(x, y, 80,80)
-    stones.push(stone)
-  }
-}
-function handleButtonPress(){
-  jointLink.detach()
-  setTimeout(()=>{
-    bridge.break()
-  },1500)
   
+
+ 
+   
 }
